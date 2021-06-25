fdf7cd041d5292a36d45599e7c92c34adc90aa08
Vanessa Diana <vanessa.d17@gmail.com>
2021-06-24 21:47:12 -0300

    aula03 - autenticação

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import { Home } from "./pages/Home";
+import { Route, BrowserRouter, Switch } from 'react-router-dom'
+import { Home } from './pages/Home';
 import { NewRoom } from "./pages/NewRoom";
+import { AuthContextProvider } from './contexts/AuthContext'
+import { Room } from './pages/Room';
 
 function App() {
-  return (
 
-    <NewRoom />
+  return (
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" exact component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+        </Switch>
+      </AuthContextProvider>
+
+    </BrowserRouter>
 
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,49 @@
+import { useHistory } from 'react-router'
+import { useAuth } from '../hooks/UseAuth'
+import { FormEvent } from 'react'
+import { useState } from 'react'
+
+import { database } from '../services/firebase'
+
+import { Button } from '../components/Button'
+import '../styles/auth.scss'
 import ilustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImage from '../assets/images/google-icon.svg'
 
-import '../styles/auth.scss'
-import { Button } from '../components/Button'
 
 export function Home() {
+
+    const history = useHistory();
+
+    const {user, signInWithGoogle } = useAuth()
+
+    const [ roomCode, setRoomCode] = useState('')
+
+   async function handleCreateRoom() {
+        if(!user) {
+          await signInWithGoogle()
+        }
+
+        history.push('/rooms/new');
+    }
+
+    async function handleJoinRoom(event:FormEvent) {
+        event.preventDefault();
+
+        if(roomCode.trim() === ''){
+            return;
+        }
+        const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+        if(!roomRef.exists()){
+            alert('Room does not exists');
+            return;
+        }
+
+        history.push(`/rooms/${roomCode}`)
+    }
+
     return (
         <div id='page-auth'>
             <aside>
@@ -16,15 +54,17 @@ export function Home() {
             <main>
                 <div className='main-content'>
                     <img src={logoImg} alt="letmeask" />
-                    <button className='create-room'>
+                    <button className='create-room' onClick={handleCreateRoom}>
                         <img src={googleIconImage} alt="logo do google" />
                         Crie sua sala com google
                     </button>
                     <div className='separator'>ou entre em uma sala</div>
-                    <form action="">
+                    <form onSubmit={handleJoinRoom}>
                         <input
                             type="text"
                             placeholder="Digite o código da sala"
+                            onChange={event=>setRoomCode(event.target.value)}
+                            value={roomCode}
                         />
                         <Button type="submit">
                             Entrar na sala
